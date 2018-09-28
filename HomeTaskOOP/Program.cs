7b3df071fa0e24cf03a5ce5eb72902627130b649
Viktor Speranskyy <vencedor@ukr.net>
2018-09-28 14:41:29 +0300

    Corrected

---
+++ HomeTaskOOP/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeTaskOOP
+{
+    class BookProgram
+    {
+        static void Main(string[] args)
+        {
+            Book book1 = new Book();
+            book1.Author = "Aleister Crowley";
+            book1.Title = "The Book of the Law.";
+            book1.Pages = 128;
+            book1.Genre = "occultism";
+            book1.PublishingHouse = "Weiser Books";
+            book1.Language = "english";
+
+            Console.WriteLine(book1.ToString());
+        }
+    }
+}
---
+++ HomeTaskOOP/book.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace HomeTaskOOP
+{
+    class Book
+    {
+        string author;
+        string title;
+        int pages;
+        string genre;
+        string publishingHouse;
+        string language;
+
+        public Book()
+        {
+        }
+
+        public Book(string author, string title, int pages, string genre, string publishingHouse, string language)
+        {
+            Author = "Aleister Crowley";
+            Title = "The Book of the Law.";
+            Pages = 128;
+            Genre = "occultism";
+            PublishingHouse = "Weiser Books";
+            Language = "english";
+        }
+
+        public string Author { get => author; set => author = value; }
+        public string Title { get => title; set => title = value; }
+        public int Pages { get => pages; set => pages = value; }
+        public string Genre { get => genre; set => genre = value; }
+        public string PublishingHouse { get => publishingHouse; set => publishingHouse = value; }
+        public string Language { get => language; set => language = value; }
+
+        public void GetInfo()
+        {
+            Console.Write(Author + " ", Title + " ", Pages + " ", Genre + " ", PublishingHouse + " ", Language + " ");
+        }
+
+        public override string ToString()
+        {
+            return Author + " " + Title + " " + Pages + " " + Genre + " " + PublishingHouse + " " + Language + " ";
+        }
+    }    
+}
